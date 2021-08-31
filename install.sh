b35c5321b21396b2b27052b2e14fcaab2aeecb71
Mark Tomcza <marktomcza@Mark-Tomcza-Mac.local>
2021-08-31 12:05:06 -0500

    clean up for springone

---
+++ install.sh
@@ -0,0 +1,11 @@
+# install git
+sudo apt install git-all
+
+# install azure cli
+curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
+
+# install azure cli spring cloud extension
+az extension add --name spring-cloud
+
+# install java openjdk-8
+sudo apt-get install openjdk-8-jdk
---
+++ verify.sh
@@ -0,0 +1,12 @@
+# verify git install
+git version
+
+# verify az install
+az help
+
+# verify az spring cloud extension installation
+az spring-cloud show –name “mark-spring-cloud-test” –resource-group “azure-spring-cloud-sa”
+
+# verify java installation
+java -version
+javac -version
